e9ce28183fd0275a06f96986f5b9b26d7f8a53a5
Thomas Kieffer <thomaskieffer@web.de>
2017-07-07 00:03:30 +0200

    Add passau coordinates and use lowercase city names

--- get_events.py
+++ get_events.py
@@ -17,6 +17,11 @@ munich = (["48.131726", "11.549377"],
           ["48.139745", "11.623192"],
           ["48.112016", "11.598473"])
 
+passau = (["48.565816", "13.413019"],
+          ["48.573654", "13.455248"])
+
+cities = (munich, passau)
+
 url = "http://localhost:3000/events"
 
 dynamodb = boto3.resource(
@@ -27,71 +32,71 @@ dynamodb = boto3.resource(
 table = dynamodb.Table('events')
 
 with table.batch_writer(overwrite_by_pkeys=['id', 'startTime']) as batch:
-  for stop in stops(freq=delorean.DAILY, count=7):
+    for stop in stops(freq=delorean.DAILY, count=7):
+        since = int(Delorean(stop.start_of_day).epoch)
+        until = int(Delorean(stop.end_of_day).epoch)
 
-      since = int(Delorean(stop.start_of_day).epoch)
-      until = int(Delorean(stop.end_of_day).epoch)
+        for city in cities:
+            for lat, lng in city:
 
-      for lat, lng in munich:
+                payload = {"lat": lat, "lng": lng, "distance": "2000",
+                           "sort": "time", "since": since, "until": until}
 
-          payload = {"lat": lat, "lng": lng, "distance": "2000",
-                     "sort": "time", "since": since, "until": until}
+                print(str(payload))
 
-          print(str(payload))
+                r = requests.get(url, params=payload)
+                data = r.json()
 
-          r = requests.get(url, params=payload)
-          data = r.json()
+                if "events" in data:
+                    events = data["events"]
 
-          if "events" in data:
-            events = data["events"]
+                    print("Found " + str(len(events)) + " events")
 
-            print("Found " + str(len(events)) + " events")
+                    for event in events:
 
-            for event in events:
+                        id = event["id"]
+                        name = event["name"]
+                        description = event["description"]
 
-              id = event["id"]
-              name = event["name"]
-              description = event["description"]
+                        startTime = parser.parse(event["startTime"]).astimezone(pytz.utc).isoformat()
 
-              startTime = parser.parse(event["startTime"]).astimezone(pytz.utc).isoformat()
+                        endTime = event["endTime"]
+                        if endTime:
+                            endTime = parser.parse(event["endTime"]).astimezone(pytz.utc).isoformat()
 
-              endTime = event["endTime"]
-              if endTime:
-                endTime = parser.parse(event["endTime"]).astimezone(pytz.utc).isoformat()
+                        imageUrl = event["profilePicture"]
+                        category = event["category"]
 
-              imageUrl = event["profilePicture"]
-              category = event["category"]
+                        zip = " "
+                        street = " "
+                        city = " "
 
-              zip = " "
-              street = " "
-              city = " "
+                        location = event["venue"]["location"]
 
-              location = event["venue"]["location"]
+                        if "street" in location:
+                            street = location["street"]
 
-              if "street" in location:
-                street = location["street"]
+                        if "zip" in location:
+                            zip = location["zip"]
 
-              if "zip" in location:
-                zip = location["zip"]    
+                        if "city" in location:
+                            city = location["city"].lower()
 
-              if "city" in location:
-                city = location["city"] 
+                        venue = {
+                            'name': event["venue"]["name"],
+                            'zip': zip,
+                            'street': street
+                        }
 
-              venue = {
-                'name': event["venue"]["name"],
-                'zip': zip,
-                'street': street
-              }
-
-              batch.put_item(
-                Item={
-                  'id': id,
-                  'city': city,
-                  'description': description,
-                  'imageUrl': imageUrl,
-                  'name': name,
-                  'startTime': startTime,
-                  'endTime': endTime,
-                  'category': str(category),
-                  'venue': venue
-              })
+                        batch.put_item(
+                            Item={
+                                'id': id,
+                                'city': city,
+                                'description': description,
+                                'imageUrl': imageUrl,
+                                'name': name,
+                                'startTime': startTime,
+                                'endTime': endTime,
+                                'category': str(category),
+                                'venue': venue
+                            })
