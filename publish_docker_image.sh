ca4e1e9876390b241f3df6b853b43defafdf3b71
Thomas Kieffer <thomas.kieffer@codecentric.de>
2017-07-14 15:53:59 +0200

    Set city names directly

--- publish_docker_image.sh
+++ publish_docker_image.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+eval "$(aws ecr get-login --no-include-email --region eu-central-1)"
+
 docker build . -t going-out/fbook-events
 
 docker tag going-out/fbook-events:latest 845599215609.dkr.ecr.eu-central-1.amazonaws.com/going-out/fbook-events:latest
--- scripts/get_events.py
+++ scripts/get_events.py
@@ -1,28 +1,29 @@
-import requests
-import delorean
-import json
 import boto3
-from dateutil import parser
+import delorean
 import pytz
-import datetime
-
-from delorean import stops
+import requests
+from dateutil import parser
 from delorean import Delorean
+from delorean import stops
 
-munich = (["48.131726", "11.549377"],
-          ["48.106973", "11.558304"],
-          ["48.135621", "11.576328"],
-          ["48.160818", "11.549549"],
-          ["48.170665", "11.625423"],
-          ["48.139745", "11.623192"],
-          ["48.112016", "11.598473"])
+cities = [
+    {
+        "name": "munich",
+        "coordinates": (["48.131726", "11.549377"],
+                        ["48.106973", "11.558304"],
+                        ["48.135621", "11.576328"],
+                        ["48.160818", "11.549549"],
+                        ["48.170665", "11.625423"],
+                        ["48.139745", "11.623192"],
+                        ["48.112016", "11.598473"])
+    },
+    {
+        "name": "passau",
+        "coordinates": (["48.565816", "13.413019"],
+                        ["48.573654", "13.455248"])
 
-passau = (["48.565816", "13.413019"],
-          ["48.573654", "13.455248"])
-
-cities = (munich, passau)
-
-url = "http://localhost:3000/events"
+    }
+]
 
 dynamodb = boto3.resource(
     'dynamodb',
@@ -37,14 +38,14 @@ with table.batch_writer(overwrite_by_pkeys=['id', 'startTime']) as batch:
         until = int(Delorean(stop.end_of_day).epoch)
 
         for city in cities:
-            for lat, lng in city:
+            for lat, lng in city["coordinates"]:
 
                 payload = {"lat": lat, "lng": lng, "distance": "2000",
                            "sort": "time", "since": since, "until": until}
 
                 print(str(payload))
 
-                r = requests.get(url, params=payload)
+                r = requests.get("http://localhost:3000/events", params=payload)
                 data = r.json()
 
                 if "events" in data:
@@ -69,7 +70,6 @@ with table.batch_writer(overwrite_by_pkeys=['id', 'startTime']) as batch:
 
                         zip = " "
                         street = " "
-                        city = " "
 
                         location = event["venue"]["location"]
 
@@ -78,9 +78,6 @@ with table.batch_writer(overwrite_by_pkeys=['id', 'startTime']) as batch:
 
                         if "zip" in location:
                             zip = location["zip"]
-
-                        if "city" in location:
-                            city = location["city"].lower()
 
                         venue = {
                             'name': event["venue"]["name"],
@@ -91,7 +88,7 @@ with table.batch_writer(overwrite_by_pkeys=['id', 'startTime']) as batch:
                         batch.put_item(
                             Item={
                                 'id': id,
-                                'city': city,
+                                'city': city["name"],
                                 'description': description,
                                 'imageUrl': imageUrl,
                                 'name': name,
